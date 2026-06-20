a3ffcb8ff751ac9c11e3c7361eb19c2a22abc6ec
Gulmira <gulmiraisakbaeva78@gmail.com>
2026-06-20 21:17:53 +0600

    feat: connect telegram bot to fastapi backend and fix migrations

---
+++ telegram-bot/src/config.py
@@ -0,0 +1,10 @@
+# telegram-bot/config.py
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    API_BASE_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+settings = Settings()
